ff7e8642d0e15e1f245020b03d05bce9fb67988e
Loraine <loraine@ciandt.com>
2019-07-28 11:55:18 -0300

    All the values are in constants Missing : put messages in constants and put the logic in functions

--- src/com/Constants.java
+++ src/com/Constants.java
@@ -2,13 +2,29 @@ package com;
 
 public class Constants {
 	
-	public static final String intergalatic_glob = "glob";
-	public static final String intergalatic_prok = "prok";
-	public static final String intergalatic_pish = "pish";
-	public static final String intergalatic_tegj = "tegj";
-	public static final String intergalatic_silver = "Silver";
-	public static final String intergalatic_gold = "Gold";
-	public static final String intergalatic_iron = "Iron";
+	public static final String intergalactic_glob = "glob";
+	public static final String intergalactic_prok = "prok";
+	public static final String intergalactic_pish = "pish";
+	public static final String intergalactic_tegj = "tegj";
+	public static final String intergalactic_silver = "Silver";
+	public static final String intergalactic_gold = "Gold";
+	public static final String intergalactic_iron = "Iron";
+	
+	public static final String roman_numeral_I = "I";
+	public static final String roman_numeral_V = "V";
+	public static final String roman_numeral_X = "X";
+	public static final String roman_numeral_L = "L";
+	public static final String roman_numeral_C = "C";
+	public static final String roman_numeral_D = "D";
+	public static final String roman_numeral_M = "M";
+	
+	public static final String intergalactic_string_value_silver = "17.0";
+	public static final String intergalactic_string_value_gold = "14450.0";
+	public static final String intergalactic_string_value_iron = "195.5";
+	
+	public static final double intergalactic_value_silver = 17.0;
+	public static final double intergalactic_value_gold = 14450.0;
+	public static final double intergalactic_value_iron = 195.5;
 	
 	public static final String exclamation_point = "?";
 	
--- src/com/Main.java
+++ src/com/Main.java
@@ -4,34 +4,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static Map<Character, Integer> romanToNumericMap = new HashMap<Character, Integer>() {
-        {
-            put('I', 1);
-            put('V', 5);
-            put('X', 10);
-            put('L', 50);
-            put('C', 100);
-            put('D', 500);
-            put('M', 1000);
-        }
-    };
-    
-    Map<String, Character> intergalaticToRomanMap = new HashMap<String, Character>() {
-        {
-            put("glob", 'I');
-            put("prok", 'V');
-            put("pish", 'X');
-            put("tegj", 'L');
-        }
-    };
-    
-    Map<String, Double> intergalaticMetalsValuesMap = new HashMap<String, Double>() {
-        {
-            put("Silver", 17.0);
-            put("Gold", 14450.0);
-            put("Iron", 195.5);
-        }
-    };
     
     static Map<String, String> questionsAndAnswers = new HashMap<String, String>();
     static double value = 0.0;
@@ -44,7 +16,6 @@ public class Main {
         InputStreamReader isr = new InputStreamReader(is);
         BufferedReader br = new BufferedReader(isr);
         String s = br.readLine();
-        s.toLowerCase();
         
         while (s != null) {
             romanValueMap.clear();
@@ -52,26 +23,26 @@ public class Main {
             String arr[] = s.split("((?<=:)|(?=:))|( )");
             
             for (int i = 0; i < arr.length; i++) {
-                if (arr[i].equals(Constants.intergalatic_glob)) {
-                    romanValueMap.add("I");
+                if (arr[i].equals(Constants.intergalactic_glob)) {
+                    romanValueMap.add(Constants.roman_numeral_I);
                     
-                } else if (arr[i].equals(Constants.intergalatic_prok)) {
-                    romanValueMap.add("V");
+                } else if (arr[i].equals(Constants.intergalactic_prok)) {
+                    romanValueMap.add(Constants.roman_numeral_V);
                     
-                } else if (arr[i].equals(Constants.intergalatic_pish)) {
-                    romanValueMap.add("X");
+                } else if (arr[i].equals(Constants.intergalactic_pish)) {
+                    romanValueMap.add(Constants.roman_numeral_X);
                     
-                } else if (arr[i].equals(Constants.intergalatic_tegj)) {
-                    romanValueMap.add("L");
+                } else if (arr[i].equals(Constants.intergalactic_tegj)) {
+                    romanValueMap.add(Constants.roman_numeral_L);
                     
-                } else if (arr[i].equals(Constants.intergalatic_silver)) {
-                    romanValueMap.add("17.0");
+                } else if (arr[i].equals(Constants.intergalactic_silver)) {
+                    romanValueMap.add(Constants.intergalactic_string_value_silver);
                     
-                } else if (arr[i].equals(Constants.intergalatic_gold)) {
-                    romanValueMap.add("14450.0");
+                } else if (arr[i].equals(Constants.intergalactic_gold)) {
+                    romanValueMap.add(Constants.intergalactic_string_value_gold);
                     
-                } else if (arr[i].equals(Constants.intergalatic_iron)) {
-                    romanValueMap.add("195.5");
+                } else if (arr[i].equals(Constants.intergalactic_iron)) {
+                    romanValueMap.add(Constants.intergalactic_string_value_iron);
                     
                 } else if (arr[i].equals(Constants.exclamation_point)) {
                     if (romanValueMap.isEmpty()) {
@@ -95,9 +66,9 @@ public class Main {
         int l = 0;
         
         for (int size = 0; size < romanValueMap.size(); size++) {
-            if (romanValueMap.get(size).equals("V")) {
+            if (romanValueMap.get(size).equals(Constants.roman_numeral_V)) {
                 v++;
-            } else if (romanValueMap.get(size).equals("L")) {
+            } else if (romanValueMap.get(size).equals(Constants.roman_numeral_L)) {
                 l++;
             }
         }
@@ -108,28 +79,32 @@ public class Main {
         
         //Ifs everything okay, we start to calculate
         for (int size = 0; size < romanValueMap.size(); size++) {
-            if (romanValueMap.get(size).equals("17.0")) {
-                value = value * 17;
+            if (romanValueMap.get(size).equals(Constants.intergalactic_string_value_silver)) {
+                value = value * Constants.intergalactic_value_silver;
                 questionsAndAnswers.replace(s, String.valueOf(value));
                 return;
                 
-            } else if (romanValueMap.get(size).equals("14450.0")) {
-                value = value * 14450.0;
+            } else if (romanValueMap.get(size).equals(Constants.intergalactic_string_value_gold)) {
+                value = value * Constants.intergalactic_value_gold;
                 questionsAndAnswers.put(s, String.valueOf(value));
                 return;
                 
-            } else if (romanValueMap.get(size).equals("195.5")) {
-                value = value * 195.5;
+            } else if (romanValueMap.get(size).equals(Constants.intergalactic_string_value_iron)) {
+                value = value * Constants.intergalactic_value_iron;
                 questionsAndAnswers.replace(s, String.valueOf(value));
                 return;
                 
-            } else if (romanValueMap.get(size).equals("I")) {
+            } else if (romanValueMap.get(size).equals(Constants.roman_numeral_I )) {
                 if (size+1 < romanValueMap.size()){
-                    if ((romanValueMap.get(size+1) == "V") | (romanValueMap.get(size+1) == "X") ) { //we can sub
+                    if ((romanValueMap.get(size+1) == Constants.roman_numeral_V) |
+                            (romanValueMap.get(size+1) == Constants.roman_numeral_X) ) {
                         value = value - 1;
-                    } else if ((romanValueMap.get(size+1) == "L") | (romanValueMap.get(size+1) == "C") ) { //we can sub
+                        
+                    } else if ((romanValueMap.get(size+1) == Constants.roman_numeral_L) |
+                            (romanValueMap.get(size+1) == Constants.roman_numeral_C) ) {
                         questionsAndAnswers.replace(s, "We can't calculate that!");
                         return;
+                        
                     } else {
                         value = value + 1;
                     }
@@ -139,35 +114,40 @@ public class Main {
 					return;
                 }
             }
-            else if (romanValueMap.get(size).equals("V")) {
-                if (size == romanValueMap.size()) { //we can sum
+            else if (romanValueMap.get(size).equals(Constants.roman_numeral_V)) {
+                if (size == romanValueMap.size()) {
                     questionsAndAnswers.replace(s, String.valueOf(value));
                     return;
                 } else {
                     value = value + 5;
                 }
                 
-            } else if (romanValueMap.get(size).equals("X")) {
-                if (size == romanValueMap.size()) { //we can sum
+            } else if (romanValueMap.get(size).equals(Constants.roman_numeral_X)) {
+                if (size == romanValueMap.size()) {
                     questionsAndAnswers.replace(s, String.valueOf(value));
                     return;
+                    
                 } else if (size+1 <= romanValueMap.size()){
-                    if ( (romanValueMap.get(size + 1) == "L") | (romanValueMap.get(size+1) == "C")) { //we can sum
+                    if ( (romanValueMap.get(size + 1).equals(Constants.roman_numeral_L)) |
+                            (romanValueMap.get(size+1).equals(Constants.roman_numeral_C))) {
                         value = value - 10;
+                        
                     } else {
                         value = value + 10;
                     }
                 } else {
                     value = value + 10;
                 }
-            } else if (romanValueMap.get(size).equals("L")) {
-                if (size == romanValueMap.size()) { //we can sum
+            } else if (romanValueMap.get(size).equals(Constants.roman_numeral_L)) {
+                if (size == romanValueMap.size()) {
                     questionsAndAnswers.replace(s, String.valueOf(value));
                     return;
+                    
                 } else if (size+1 <= romanValueMap.size()){
-                    if ( (romanValueMap.get(size + 1) == "L")) { //we can sum
+                    if ( (romanValueMap.get(size + 1).equals(Constants.roman_numeral_L)) ) { 
                         questionsAndAnswers.replace(s, "We can't calculate that!");
                         return;
+                        
                     } else {
                         value = value + 50;
                     }
